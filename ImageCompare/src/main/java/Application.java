c6c54de7d92880a37af7993366441073b04570d1
Mark Vallis <markvallis1987@Gmail.com>
2018-05-01 23:21:07 +0100

    Image Comparison added (needs work)

--- ImageCompare/src/main/java/Application.java
+++ ImageCompare/src/main/java/Application.java
@@ -2,6 +2,6 @@ public class Application {
 
     public static void main(String[] args) {
         new BasicComparison();
-//        new AdvancedComparison();
+        new AdvancedComparison();
     }
 }
