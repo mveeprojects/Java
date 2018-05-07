5142e1d96250e535a3c06b175934817d8bba18f6
Mark Vallis <markvallis1987@Gmail.com>
2018-05-07 17:19:58 +0100

    Added some more lab sheet code from university

--- UniversityLabSheets/src/main/java/Level_1/Lab7_BuiltInFunctions/BuiltInFunctions_7_5.java
+++ UniversityLabSheets/src/main/java/Level_1/Lab7_BuiltInFunctions/BuiltInFunctions_7_5.java
@@ -18,7 +18,15 @@ class BuiltInFunctions_7_5 {
         if (dir_list != null) {
             System.out.println("Files inside \"" + dir_name + "\":");
             for (File file : dir_list) {
-                System.out.println(file.getName());
+                System.out.print(file.getName());
+                if(file.isDirectory()){
+                    System.out.println(" DIRECTORY");
+                } else {
+                    System.out.println(" FILE");
+                    if (file.getName().contains(".txt")){
+                        System.out.println(" and it is a .txt file!");
+                    }
+                }
             }
         } else {
             System.out.println("No files or directories found in \"" + dir_name + "\", also check that directory \"" + dir_name + "\" exists");
